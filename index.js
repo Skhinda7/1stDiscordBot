653f192f1c126c45967c7ce50de1401267a4d713
Skhinda7 <73970774+Skhinda7@users.noreply.github.com>
2020-11-05 17:45:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* This is a straightforward code that allows you to bring a discord bot to life
+You will need your bot token which can be acquired from the Discord Developer Portal: https://discord.com/developers/ */
 const Discord = require('discord.js');
 
 const bot = new Discord.Client();
